cb1746d1969d83f9d5d658ffbe98ba41e5ec4ddf
vs00382 <vida.sharifian.attar@gmail.com>
2025-11-21 20:35:47 +0000

    a*

--- global.gd
+++ global.gd
@@ -6,38 +6,59 @@ extends Node2D
 var astar := AStarGrid2D.new()
 
 func _ready():
-	setup_astar()
+	build_astar_from_tilemap()
 
-func setup_astar():
-	# Make grid larger than needed (fine for now)
-	astar.region = Rect2i(0, 0, 300, 300)
-
-	# Use your actual tile size
+func build_astar_from_tilemap():
+	# Use tilemap’s actual tile size
 	astar.cell_size = ground_layer.tile_set.tile_size
-
-	# Enable 8-direction connectivity
 	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ALWAYS
 
-	# Make all tiles walkable (not solid)
+	# Get all used tiles in the tilemap
+	var used := ground_layer.get_used_cells()
+	if used.is_empty():
+		push_warning("TileMapLayer has no tiles!")
+		return
+
+	# Determine bounding box of painted tiles
+	var min_x = used[0].x
+	var min_y = used[0].y
+	var max_x = used[0].x
+	var max_y = used[0].y
+
+	for c in used:
+		min_x = min(min_x, c.x)
+		min_y = min(min_y, c.y)
+		max_x = max(max_x, c.x)
+		max_y = max(max_y, c.y)
+
+	astar.region = Rect2i(min_x, min_y, max_x - min_x + 1, max_y - min_y + 1)
+
+	# Mark all walkable tiles
+	for c in used:
+		astar.set_point_solid(c, false)
+
+	# Mark all non-painted tiles inside region as blocked
 	for x in astar.region.size.x:
 		for y in astar.region.size.y:
-			astar.set_point_solid(Vector2i(x, y), false)
+			var c = Vector2i(min_x + x, min_y + y)
+			if not used.has(c):
+				astar.set_point_solid(c, true)
 
 	astar.update()
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
-		var mouse_local: Vector2 = ground_layer.to_local(event.position)
-		var target_cell: Vector2i = ground_layer.local_to_map(mouse_local)
+		var mouse_local = ground_layer.to_local(event.position)
+		var target_cell = ground_layer.local_to_map(mouse_local)
 
-		var pawn_local: Vector2 = ground_layer.to_local(pawn.global_position)
-		var start_cell: Vector2i = ground_layer.local_to_map(pawn_local)
+		var pawn_local = ground_layer.to_local(pawn.global_position)
+		var start_cell = ground_layer.local_to_map(pawn_local)
 
 		var path = astar.get_id_path(start_cell, target_cell)
 		print("PATH:", path)
 
 		if path.is_empty():
-			print("NO PATH!")
+			print("NO PATH")
 			return
 
 		pawn.follow_path(path)
