4562c8dc09ab65aa6e7549cfeb09ac3bc94e5667
vs00382 <vida.sharifian.attar@gmail.com>
2025-11-21 20:20:08 +0000

    bit better

--- global.gd
+++ global.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var ground_layer: TileMapLayer = $GroundLayer
-@onready var pawn = $Pawn
+@onready var pawn: Node2D = $Pawn
 
 var astar := AStarGrid2D.new()
 
@@ -9,25 +9,35 @@ func _ready():
 	setup_astar()
 
 func setup_astar():
-	astar.region = Rect2i(0, 0, 100, 100)  # adjust to your map size
-	astar.cell_size = Vector2(32, 32)
-	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	# Make grid larger than needed (fine for now)
+	astar.region = Rect2i(0, 0, 300, 300)
+
+	# Use your actual tile size
+	astar.cell_size = ground_layer.tile_set.tile_size
+
+	# Enable 8-direction connectivity
+	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ALWAYS
+
+	# Make all tiles walkable (not solid)
+	for x in astar.region.size.x:
+		for y in astar.region.size.y:
+			astar.set_point_solid(Vector2i(x, y), false)
+
 	astar.update()
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
-		# global → tilemap local
-		var mouse_pos_local = ground_layer.to_local(event.position)
-		var target_cell = ground_layer.local_to_map(mouse_pos_local)
+		var mouse_local: Vector2 = ground_layer.to_local(event.position)
+		var target_cell: Vector2i = ground_layer.local_to_map(mouse_local)
 
-		var pawn_local = ground_layer.to_local(pawn.global_position)
-		var start_cell = ground_layer.local_to_map(pawn_local)
-
-		print("Start:", start_cell, " Target:", target_cell)
+		var pawn_local: Vector2 = ground_layer.to_local(pawn.global_position)
+		var start_cell: Vector2i = ground_layer.local_to_map(pawn_local)
 
 		var path = astar.get_id_path(start_cell, target_cell)
+		print("PATH:", path)
 
-		if not path.is_empty():
-			pawn.follow_path(path)
-		else:
-			print("NO PATH")
+		if path.is_empty():
+			print("NO PATH!")
+			return
+
+		pawn.follow_path(path)
